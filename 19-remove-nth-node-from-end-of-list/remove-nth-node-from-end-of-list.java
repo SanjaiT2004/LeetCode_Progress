9da7ded64c13464b0d75918b41ab7dd5a9e30bea
SanjaiT2004 <139303214+SanjaiT2004@users.noreply.github.com>
2024-03-25 13:47:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (57.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -9,33 +9,55 @@
  * }
  */
 class Solution {
+    // public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     ListNode temp = head ;
+    //     int len =0;
+    //     while(temp!=null){
+    //         temp =temp.next;
+    //         len++;
+    //     }
+    //     int k = len-n;
+    //     if(k ==0){
+    //         return head.next;
+    //     }
+    //     temp = head;
+    //     ListNode prev = null;
+    //     int cnt =0;
+    //     while(temp!=null){
+    //         if(cnt == k){
+    //             break;
+    //         }
+    //         cnt++;
+    //         prev=temp;
+    //         temp=temp.next;
+    //     }
+    //     if(n==1){
+    //     prev.next=null;
+    //     }else{
+    //         prev.next = temp.next;
+    //     }
+    //     return  head;
+    // }
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode temp = head ;
-        int len =0;
-        while(temp!=null){
-            temp =temp.next;
-            len++;
+        if(head.next == null) {
+            return null;
         }
-        int k = len-n;
-        if(k ==0){
-            return head.next;
+        if(findAndRemove(head, head, n) == -1) { // delete head node
+            head = head.next;
         }
-        temp = head;
-        ListNode prev = null;
-        int cnt =0;
-        while(temp!=null){
-            if(cnt == k){
-                break;
-            }
-            cnt++;
-            prev=temp;
-            temp=temp.next;
+        return head;
+    }
+
+    public int findAndRemove(ListNode head, ListNode node, int n) {
+        if(node.next == null) {
+            return 0;
         }
-        if(n==1){
-        prev.next=null;
-        }else{
-            prev.next = temp.next;
+        int length = findAndRemove(head, node.next, n) + 1;
+        if(length == n) {
+            node.next = node.next.next; //delete node
+        }else if(head == node && length == n-1) {
+            return -1; // delete head node 
         }
-        return  head;
+        return length;
     }
 }
